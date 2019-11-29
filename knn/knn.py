31fc25cc6c54c7462e2c08db03c879be6eb4b9c9
Noam <noams@epd-medical.com>
2019-11-30 00:59:17 +0200

    almost done with knn, unique fails over axis

--- knn/knn.py
+++ knn/knn.py
@@ -1,18 +1,32 @@
 from loader.DataSet import DataSet
+from scipy.spatial.distance import cdist
+import numpy as np
 
 
 class KnnClassifier(object):
     def __init__(self, data_path):
         self._data_path = data_path
-        self._data = DataSet(self._data_path)
+        self._data_set = DataSet(self._data_path)
+        self._x_train = self._data_set.x_train
+        self._y_train = self._data_set.y_train
+        self._x_test = self._data_set.x_test
+        self._y_test = self._data_set.y_test
 
-    def run(self):
-        pass
+    def classify_test_by_train(self, k):
+        only_check_for_x_first = 100
+        only_use_first_x_from_test = 1000
+        x_test = self._x_test[:only_check_for_x_first]
+        x_train = self._x_train[:only_use_first_x_from_test]
+        pairs_distances = cdist(x_test, x_train)
+        k_min_distances_inds_per_test_sample = np.argpartition(pairs_distances, k, axis=0)[:, :k]
+        votes_per_test_sample = self._y_train[k_min_distances_inds_per_test_sample]
+        classes, votes_count = np.unique(votes_per_test_sample, return_counts=True, axis=0)
+        hi=5
 
 
 if __name__ == "__main__":
     def main():
         classifier = KnnClassifier(data_path='../mnist.pkl.gz')
-        classifier.run()
+        classifier.classify_test_by_train(k=5)
 
     main()
