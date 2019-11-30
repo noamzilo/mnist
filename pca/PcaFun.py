69f59232cd6d26814de82d042778c37c18640d11
Noam <noams@epd-medical.com>
2019-11-30 19:51:57 +0200

    more progress, got to dimensionality reduction

--- pca/PcaFun.py
+++ pca/PcaFun.py
@@ -8,10 +8,10 @@ class PcaFun(object):
         self._data_path = data_path
         self._data_set = DataSet(self._data_path)
         self._shape = self._data_set.shape
-        # train_data_size = len(self._data_set.y_train)
-        # test_data_size = len(self._data_set.y_test)
-        train_data_size = 1000
-        test_data_size = 100
+        train_data_size = len(self._data_set.y_train)
+        test_data_size = len(self._data_set.y_test)
+        # train_data_size = 1000
+        # test_data_size = 100
         self._train_subset_size = train_data_size
         self._test_subset_size = test_data_size
         self._x_train = self._data_set.x_train[:self._train_subset_size]
@@ -51,12 +51,13 @@ class PcaFun(object):
         diff_numeric = abs_diff < eps
         assert all(diff_numeric)
         plt.figure()
+        plt.title('average component')
         plt.imshow(np.reshape(avg_digit_original, self._shape), cmap='gray', interpolation='nearest')
         plt.show(block=True)
 
     def draw_first_k_components(self, k, rows, cols):
         pca = self._pca
-        first_k_pc_restored = self._reverse_transform(pca.components_[:k])
+        first_k_pc_restored = pca.components_[:k]  # components are already in feature space by sklearn
 
         assert cols * rows == k
         fig, plots = plt.subplots(nrows=rows, ncols=cols)
@@ -72,9 +73,42 @@ class PcaFun(object):
         plt.subplots_adjust(left=None, bottom=.1, right=None, top=0.95, wspace=None, hspace=None)
         plt.show()
 
+    def draw_cumulative_variance_graph(self):
+        pca = self._pca
+        explained_variance_ratio = pca.explained_variance_ratio_
+        plt.figure()
+        plt.title('cumulative explained variance ratio')
+        plt.xlabel('#components')
+        plt.ylabel('cumulative explained_variance_ratio')
+        plt.plot(np.cumsum(explained_variance_ratio))
+        plt.show(block=True)
 
+    def calculate_n_components_required(self, desired_variance_ratio):
+        pca = self._pca
+        explained_variance_ratio = pca.explained_variance_ratio_
+        cumulative_explained_variance_ratio = np.cumsum(explained_variance_ratio)
+        cutoff_ind = np.searchsorted(cumulative_explained_variance_ratio, desired_variance_ratio) + 1
+        return cutoff_ind
 
-# explained_variance_ratio = pca.explained_variance_ratio_
+    def reduce_dimensionality_to_2_and_plot(self):
+        pca = self._pca
+        train_pc = pca.transform(self._x_train)
+        train_pc_x_component = train_pc[:, 0]
+        train_pc_y_component = train_pc[:, 1]
+
+        labels = self._y_train
+        # colors = ['r', 'g', 'b', 'm', 'y', 't', 'k', (255, 125, 40), (68, 202, 225), (82, 255, 38)]
+
+        plt.figure()
+        plt.title('dimensionality reduction to 2')
+
+        for i, label in enumerate(np.unique(labels)):
+            label_ind = np.where(labels == label)
+            plt.scatter(train_pc_x_component[label_ind], train_pc_y_component[label_ind],
+                        marker='.',
+                        c=np.random.rand(3,))
+        plt.show(block=True)
+
 
 if __name__ == "__main__":
     pca_fun = PcaFun(data_path='../mnist.pkl.gz')
@@ -82,3 +116,9 @@ if __name__ == "__main__":
     pca_fun.sanity_restore_data()
     pca_fun.draw_average_digit()
     pca_fun.draw_first_k_components(k=6, rows=2, cols=3)
+    pca_fun.draw_cumulative_variance_graph()
+    cutoff_for_80_percent = pca_fun.calculate_n_components_required(desired_variance_ratio=0.8)
+    cutoff_for_95_percent = pca_fun.calculate_n_components_required(desired_variance_ratio=0.95)
+    print(f"cutoff_for_80_percent: {cutoff_for_80_percent}")
+    print(f"cutoff_for_95_percent: {cutoff_for_95_percent}")
+    pca_fun.reduce_dimensionality_to_2_and_plot()
